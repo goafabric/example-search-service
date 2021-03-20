d89ce8cd4cac53ab87fb19f2f98a1024e9667256
Andreas Mautsch <amautsch>
2021-03-20 14:18:06 +0100

    example-search-service

---
+++ src/main/java/org/goafabric/example/searchservice/configuration/ElasticClientConfig.java
@@ -0,0 +1,32 @@
+package org.goafabric.example.searchservice.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Slf4j
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "org.goafabric.example.searchservice.persistence")
+public class ElasticClientConfig {
+
+    @Bean
+    public RestHighLevelClient client() {
+        ClientConfiguration clientConfiguration
+            = ClientConfiguration.builder()
+            .connectedTo("localhost:9200")
+            .build();
+        log.info("Elasticsearch has started.");
+        return RestClients.create(clientConfiguration).rest();
+    }
+
+    /*
+    @Bean
+    public ElasticsearchOperations elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(client());
+    }
+     */
+}
--- src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
+++ src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
@@ -5,15 +5,25 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Document(indexName = "persons")
 public class PersonBo {
     @Id
     private String id;
 
+    @Field(type = FieldType.Text)
     private String firstName;
+
+    @Field(type = FieldType.Text)
     private String lastName;
+
+    //@Field(type = FieldType.Keyword)
+    //private String userId;
 }
