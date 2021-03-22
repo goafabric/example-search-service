9c6dede026eee700ef870ae669a3cb82515ece72
Andreas Mautsch <amautsch>
2021-03-22 13:43:08 +0100

    tenant added

---
+++ src/main/java/org/goafabric/example/searchservice/configuration/TenantRequestContext.java
@@ -0,0 +1,27 @@
+/*
+package org.goafabric.example.searchservice.configuration;
+
+public final class TenantRequestContext {
+    private TenantRequestContext() {
+    }
+
+    private static final ThreadLocal<String> tenantIdThreadLocal = new ThreadLocal<>();
+
+    public static String getTenantId() {
+        String tenantId = tenantIdThreadLocal.get();
+        if (tenantId == null) {
+            tenantId = "10"; //TODO: warning this is just a temp hack to make the demo frontend work without http headders
+            //throw new IllegalStateException("tenantId is null");
+        }
+        return tenantId;
+    }
+
+    static void setTenantId(String tenantId) {
+        tenantIdThreadLocal.set(tenantId);
+    }
+
+    static void remove() {
+        tenantIdThreadLocal.remove();
+    }
+}
+ */
