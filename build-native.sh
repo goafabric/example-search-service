c39e736cda92f28f1f41931730d589edd57e2fe1
Andreas Mautsch <amautsch>
2021-03-28 11:46:47 +0200

    changes

--- build-native.sh
+++ build-native.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git pull
-time mvn clean spring-boot:build-image install
+time mvn clean spring-boot:build-image
+#time mvn clean spring-boot:build-image install
 docker run --name example-search-service --rm -p50800:50800 -e "elasticsearch.url=172.17.0.1:9200" goafabric/example-search-service-native:1.0.1-SNAPSHOT -Xmx64m
