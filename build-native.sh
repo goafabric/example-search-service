132a7365257d159cc32344ec0c5789f36b4062d5
Andreas Mautsch <amautsch>
2021-03-29 09:20:57 +0200

    native-profile

--- build-native.sh
+++ build-native.sh
@@ -2,4 +2,4 @@
 git pull
 time mvn clean spring-boot:build-image -P docker-image-native
 #time mvn clean spring-boot:build-image install -P docker-image-native
-docker run --name example-search-service --rm -p50800:50800 -e "elasticsearch.url=172.17.0.1:9200" goafabric/example-search-service-native:1.0.1-SNAPSHOT -Xmx64m
+docker run --name example-search-service --rm -p50800:50800 -e "elasticsearch.url=172.17.0.1:9200" goafabric/example-search-service-native:1.0.2-SNAPSHOT -Xmx64m
--- doc/release-notes.md
+++ doc/release-notes.md
@@ -1,3 +1,5 @@
+#1.0.2
+
 #1.0.1
 - native image via profile added
 - kubernetes and docker compose files added
