f114f6485c5d813ece2517076d38685c4da5a7d7
Andreas Mautsch <amautsch>
2021-08-28 11:02:39 +0200

    profile mgmt

--- build-native.sh
+++ build-native.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 git pull
-time mvn clean spring-boot:build-image -P docker-image-native
-#time mvn clean spring-boot:build-image install -P docker-image-native
+time mvn clean deploy -P docker-image-native
 docker run --name example-search-service --rm -p50800:50800 -e "elasticsearch.url=172.17.0.1:9200" goafabric/example-search-service-native:1.0.3-SNAPSHOT -Xmx64m
