f75b90cd39f6b3ff38e4890b783370589bb47e19
Andreas Mautsch <amautsch>
2021-12-17 14:46:22 +0100

    mongodb added

--- src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
+++ src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 //@Document(indexName = "#{@tenantIdBean.getPrefix()}persons")
+@Document("#{@tenantIdBean.getPrefix()}persons")
 public class PersonBo {
     @Id
     private String id;
