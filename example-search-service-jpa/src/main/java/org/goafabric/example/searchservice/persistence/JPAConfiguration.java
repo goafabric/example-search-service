47eaf6faf471b6ba6b3ed88ba780be0aa2183590
Andreas Mautsch <amautsch>
2021-12-17 16:26:22 +0100

    multi module refactoring

--- example-search-service-jpa/src/main/java/org/goafabric/example/searchservice/persistence/JPAConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.goafabric.example.searchservice.persistence;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-@Configuration
-@EnableJpaRepositories(basePackages = "org.goafabric.example.searchservice.persistence")
-public class JPAConfiguration {
-}
--- example-search-service-jpa/src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
+++ example-search-service-jpa/src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
@@ -4,17 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
+import org.hibernate.annotations.GenericGenerator;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table
+@Entity
+@Table(name = "person")
 public class PersonBo {
     @Id
+    @GeneratedValue(generator = "uuid") @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
     private String firstName;
