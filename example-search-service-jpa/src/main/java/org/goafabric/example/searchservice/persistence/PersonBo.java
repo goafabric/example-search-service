a40ff873d3b0572a95012e451f666f1080a3cb66
Andreas Mautsch <amautsch>
2021-12-18 17:36:14 +0100

    person extensions

--- example-search-service-jpa/src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
+++ example-search-service-jpa/src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
@@ -1,28 +1,44 @@
 package org.goafabric.example.searchservice.persistence;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "person")
+@Table(name="person")
 public class PersonBo {
     @Id
-    @GeneratedValue(generator = "uuid") @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
+    
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    @NonNull
+    private AddressBo address;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.ALL}, mappedBy = "person")
+    @NonNull
+    private List<SkillBo> skills;
+
+    //what a shoehorn of hoseshit is this ?!?!? i have to do this myself ?!
+    public void setSkills(List<SkillBo> skills) {
+        if (skills != null) {
+            skills.stream().forEach(skill -> skill.setPerson(this));
+            this.skills = skills;
+        }
+    }
 }
+
---
+++ example-search-service-jpa/src/main/java/org/goafabric/example/searchservice/persistence/SkillBo.java
@@ -0,0 +1,31 @@
+package org.goafabric.example.searchservice.persistence;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="skill")
+public class SkillBo {
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+
+    private String name;
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "person_id", referencedColumnName = "id")
+    private PersonBo person;
+}
