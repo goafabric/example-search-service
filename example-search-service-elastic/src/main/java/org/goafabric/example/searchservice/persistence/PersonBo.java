20d6e314aedcad451fb3150e3446e5b356998a11
Andreas Mautsch <amautsch>
2021-12-18 17:30:14 +0100

    person extensions

--- example-search-service-elastic/src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
+++ example-search-service-elastic/src/main/java/org/goafabric/example/searchservice/persistence/PersonBo.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.util.List;
 
 @Data
 @Builder
@@ -16,9 +20,13 @@ public class PersonBo {
     @Id
     private String id;
 
-    //@Field(type = FieldType.Text)
     private String firstName;
 
-    //@Field(type = FieldType.Text)
     private String lastName;
+
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private AddressBo address;
+
+    @Field(type = FieldType.Nested, includeInParent = true)
+    private List<SkillBo> skills;
 }
---
+++ example-search-service-elastic/src/main/java/org/goafabric/example/searchservice/persistence/SkillBo.java
@@ -0,0 +1,22 @@
+package org.goafabric.example.searchservice.persistence;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "#{@tenantIdBean.getPrefix()}skill")
+public class SkillBo {
+    @Id
+    private String id;
+
+    private String name;
+    private String description;
+}
