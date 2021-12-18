c7178139d923fdc65032be3101aaf46e94e0b5ed
Andreas Mautsch <amautsch>
2021-12-18 17:21:49 +0100

    person extensions

--- example-search-service-mongo/src/main/java/org/goafabric/example/searchservice/persistence/PersonRepository.java
+++ example-search-service-mongo/src/main/java/org/goafabric/example/searchservice/persistence/PersonRepository.java
@@ -1,11 +1,15 @@
 package org.goafabric.example.searchservice.persistence;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PersonRepository extends PagingAndSortingRepository<PersonBo, String> {
     List<PersonBo> findByFirstName(String firstName);
     List<PersonBo> findByLastName(String lastName);
+
+    List<PersonBo> findByLastNameStartsWithIgnoreCase(String lastName);
+    List<PersonBo> findByAddress_City(@Param("city") String city);
 }
 
