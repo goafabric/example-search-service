1f9363ad04b558665b70eafd54784be4f47fe021
Andreas Mautsch <amautsch>
2022-03-03 09:30:19 +0100

    - upgrade to Spring Boot 2.6.4 - upgrade to Spring Native 0.11.3

--- build-native.sh
+++ build-native.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 time mvn clean deploy -P docker-image-native
-docker run --name example-search-service --rm -p50800:50800 -e "elasticsearch.url=172.17.0.1:9200" goafabric/example-search-service-native:1.1.1-SNAPSHOT -Xmx64m
+docker run --name example-search-service --rm -p50800:50800 -e "elasticsearch.url=172.17.0.1:9200" goafabric/example-search-service-native:1.1.1-upgrade-SNAPSHOT -Xmx64m
--- doc/release-notes.md
+++ doc/release-notes.md
@@ -1,6 +1,6 @@
 #1.1.1
-- upgrade to Spring Boot 2.6.3
-- upgrade to Spring Native 0.11.1
+- upgrade to Spring Boot 2.6.4
+- upgrade to Spring Native 0.11.3
 - Update to OpenAPI 1.6.4 + JIB 3.2.0
 
 - Java 17 IBM Semeru Runtime for JVM Images
