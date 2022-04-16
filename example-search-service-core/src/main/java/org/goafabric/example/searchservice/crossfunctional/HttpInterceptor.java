b472fd18b2fbc24aba6781862f34f6ac4e54060f
Andreas Mautsch <amautsch>
2022-04-16 10:41:38 +0200

    upgrade

--- example-search-service-core/src/main/java/org/goafabric/example/searchservice/crossfunctional/HttpInterceptor.java
+++ example-search-service-core/src/main/java/org/goafabric/example/searchservice/crossfunctional/HttpInterceptor.java
@@ -1,5 +1,6 @@
 package org.goafabric.example.searchservice.crossfunctional;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -9,32 +10,31 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Configuration
-public class TenantIdInterceptor implements WebMvcConfigurer {
-    private static final ThreadLocal<String> tenantIdThreadLocal = new ThreadLocal<>();
+@Slf4j
+public class HttpInterceptor implements WebMvcConfigurer {
+    private static final ThreadLocal<String> tenantId = new ThreadLocal<>();
+    private static final ThreadLocal<String> userName = new ThreadLocal<>();
+
+    public static String getTenantId() { return tenantId.get(); }
+    public static String getUserName() { return userName.get(); }
+    public static void   setTenantId(String tenantId) { HttpInterceptor.tenantId.set(tenantId); }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new HandlerInterceptor() {
             @Override
             public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-                setTenantId(request.getHeader("X-TenantId"));
+                tenantId.set(request.getHeader("X-TenantId") != null ? request.getHeader("X-TenantId") : "0"); //TODO
+                userName.set(request.getHeader("X-Auth-Request-Preferred-Username") != null ? request.getHeader("X-Auth-Request-Preferred-Username")
+                                                :  "");
                 return true;
             }
 
             @Override
             public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-                tenantIdThreadLocal.remove();
+                tenantId.remove();
+                userName.remove();
             }
         });
     }
-
-    public static String getTenantId() {
-        final String tenantId = tenantIdThreadLocal.get();
-        return tenantId == null ? "0" : tenantId;  //Todo: should throw exception
-    }
-
-    public static void setTenantId(String tenantId) {
-        tenantIdThreadLocal.set(tenantId);
-    }
-
 }
