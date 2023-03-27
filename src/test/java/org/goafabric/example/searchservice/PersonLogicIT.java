6053e85f8fa344850e24a2ba5d33625d59a62106
Andreas Mautsch <amautsch>
2023-03-27 14:01:05 +0200

    changes

--- src/test/java/org/goafabric/example/searchservice/PersonLogicIT.java
+++ src/test/java/org/goafabric/example/searchservice/PersonLogicIT.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ExtendWith(TestContainerExtension.class)
-class PersonLogicNRIT {
+class PersonLogicIT {
     @Autowired
     private PersonLogic personLogic;
 
