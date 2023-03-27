228f713e63f4fac15202fdce4b1a0e23b0fb1208
Andreas Mautsch <amautsch>
2023-03-27 08:43:49 +0200

    testcontainer

--- src/test/java/org/goafabric/example/searchservice/ElasticTestContainer.java
+++ src/test/java/org/goafabric/example/searchservice/ElasticTestContainer.java
@@ -2,14 +2,16 @@ package org.goafabric.example.searchservice;
 
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
-import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.HashMap;
 
 public class ElasticTestContainer implements BeforeAllCallback {
 
-    private static final ElasticsearchContainer container =
-            new ElasticsearchContainer("docker.elastic.co/elasticsearch/elasticsearch:8.6.1")
+    private static final GenericContainer container =
+            new GenericContainer(
+                    DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch").withTag("8.6.1"))
                     .withExposedPorts(9200, 9300)
                     .withEnv(new HashMap<>()  {{
                         put("ES_JAVA_OPTS", "-Xms256m -Xmx256m");
@@ -18,7 +20,7 @@ public class ElasticTestContainer implements BeforeAllCallback {
                         put("xpack.security.transport.ssl.enabled", "false");
                         put("xpack.security.http.ssl.enabled", "false");
                     }});
-    
+
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
         if (!container.isCreated()) {
@@ -26,8 +28,9 @@ public class ElasticTestContainer implements BeforeAllCallback {
             System.setProperty("spring.elasticsearch.uris", "http://localhost:" + container.getMappedPort(9200));
         }
     }
+
 }
 
 
-//https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.1.0-M2-Release-Notes
+// https://github.com/spring-projects/spring-boot/wiki/Spring-Boot-3.1.0-M2-Release-Notes
 //https://www.baeldung.com/spring-dynamicpropertysource
