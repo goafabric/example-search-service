deb3199fe23544099888b165807c58b58548b8fa
Andreas Mautsch <amautsch>
2023-03-27 08:54:26 +0200

    testcontainer

--- src/test/java/org/goafabric/example/searchservice/ElasticTestContainer.java
+++ src/test/java/org/goafabric/example/searchservice/ElasticTestContainer.java
@@ -9,24 +9,25 @@ import java.util.HashMap;
 
 public class ElasticTestContainer implements BeforeAllCallback {
 
-    private static final GenericContainer container =
-            new GenericContainer(
-                    DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch").withTag("8.6.1"))
-                    .withExposedPorts(9200, 9300)
-                    .withEnv(new HashMap<>()  {{
-                        put("ES_JAVA_OPTS", "-Xms256m -Xmx256m");
-                        put("discovery.type", "single-node");
-                        put("xpack.security.enabled", "false");
-                        put("xpack.security.transport.ssl.enabled", "false");
-                        put("xpack.security.http.ssl.enabled", "false");
-                    }});
+    private static GenericContainer container;
+
+    static {
+        container = new GenericContainer(
+                DockerImageName.parse("docker.elastic.co/elasticsearch/elasticsearch").withTag("8.6.1"))
+                .withExposedPorts(9200, 9300)
+                .withEnv(new HashMap<>() {{
+                    put("ES_JAVA_OPTS", "-Xms256m -Xmx256m");
+                    put("discovery.type", "single-node");
+                    put("xpack.security.enabled", "false");
+                    put("xpack.security.transport.ssl.enabled", "false");
+                    put("xpack.security.http.ssl.enabled", "false");
+                }});
+        container.start();
+    }
 
     @Override
     public void beforeAll(ExtensionContext context) throws Exception {
-        if (!container.isCreated()) {
-            container.start();
-            System.setProperty("spring.elasticsearch.uris", "http://localhost:" + container.getMappedPort(9200));
-        }
+        System.setProperty("spring.elasticsearch.uris", "http://localhost:" + container.getMappedPort(9200));
     }
 
 }
