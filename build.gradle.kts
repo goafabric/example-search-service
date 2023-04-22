b06347c943d96d3d5c01c054368af39478013450
Andreas Mautsch <amautsch>
2023-04-22 11:13:07 +0200

    upgrade to 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 dependencies {
 	//web
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-graphql")
+	//implementation("org.springframework.boot:spring-boot-starter-graphql")
 
 	//persistence
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
