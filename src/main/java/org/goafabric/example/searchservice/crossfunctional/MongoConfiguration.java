f4da43f2846b8a9e1db2480a5f47b87bc33a7284
Andreas Mautsch <amautsch>
2023-05-23 08:11:15 +0200

    changes

--- src/main/java/org/goafabric/example/searchservice/crossfunctional/MongoConfiguration.java
+++ src/main/java/org/goafabric/example/searchservice/crossfunctional/MongoConfiguration.java
@@ -2,11 +2,15 @@ package org.goafabric.example.searchservice.crossfunctional;
 
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 @Profile("mongodb")
@@ -14,11 +18,14 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 public class MongoConfiguration {
 
     @Configuration
+    @ConditionalOnMissingClass("de.flapdoodle.embed.mongo.spring.autoconfigure.EmbeddedMongoAutoConfiguration")
     static class MongoClientConfig extends AbstractMongoClientConfiguration {
         @Value("${spring.data.mongodb.authentication-database}") private String authDb;
         @Value("${spring.data.mongodb.authentication-database}") private String db;
         @Value("${spring.data.mongodb.username}") private String user;
         @Value("${spring.data.mongodb.password}") private String password;
+        @Value("${spring.data.mongodb.host:localhost}") private String host;
+        @Value("${spring.data.mongodb.port:27017}") private Integer port;
 
         @Override
         protected String getDatabaseName() {
@@ -27,8 +34,10 @@ public class MongoConfiguration {
 
         @Override
         protected void configureClientSettings(MongoClientSettings.Builder builder) {
-            builder.credential(MongoCredential.createCredential(user, authDb, password.toCharArray()));
+            builder.credential(MongoCredential.createCredential(user, authDb, password.toCharArray()))
+                    .applyToClusterSettings(settings -> {
+                        settings.hosts(singletonList(new ServerAddress(host, port)));
+                    });
         }
     }
-
 }
