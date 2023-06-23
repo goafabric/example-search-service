7d828c3444cc7381d806ac382de15bd6f478953c
Andreas Mautsch <amautsch>
2023-06-23 10:32:43 +0200

    upgrade to boot 3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,4 @@ tasks.named<BootBuildImage>("bootBuildImage") {
 		exec { commandLine("docker", "push", nativeImageName) }
 	}
 }
-
-graalvmNative { //https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html#configuration-options
-	binaries.named("main") { quickBuild.set(true) }
-}
+graalvmNative { binaries.named("main") { quickBuild.set(true) } }
