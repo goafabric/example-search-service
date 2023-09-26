a187eab8751840a1acf1c3ae644db53d4e8c0b30
Andreas Mautsch <amautsch>
2023-09-26 17:00:34 +0200

    autoconfig changes

--- src/main/java/org/goafabric/example/searchservice/persistence/entity/PersonBo.java
+++ src/main/java/org/goafabric/example/searchservice/persistence/entity/PersonBo.java
@@ -2,12 +2,13 @@ package org.goafabric.example.searchservice.persistence.entity;
 
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.util.List;
 
 @org.springframework.data.mongodb.core.mapping.Document("#{@tenantIdBean.getPrefix()}person")
 @org.springframework.data.elasticsearch.annotations.Document(indexName = "#{@tenantIdBean.getPrefix()}person", createIndex = false)
-//@RedisHash("address")
+@RedisHash("address")
 public record PersonBo(
     @Id
     String id,
