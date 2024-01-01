7c63d0fb7eebeb3fb3b33549a177dc4101a6d9b7
Andreas Mautsch <amautsch>
2024-01-01 11:54:40 +0100

    lucene added

--- src/test/java/org/goafabric/example/searchservice/lucene/InMemoryLuceneIndex.java
+++ src/test/java/org/goafabric/example/searchservice/lucene/InMemoryLuceneIndex.java
@@ -6,13 +6,14 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.SortedDocValuesField;
 import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.*;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.util.BytesRef;
@@ -64,9 +65,10 @@ public class InMemoryLuceneIndex {
 
     public List<Document> searchIndex(Query query) {
         try {
-            IndexReader indexReader = DirectoryReader.open(memoryIndex);
-            IndexSearcher searcher = new IndexSearcher(indexReader);
-            TopDocs topDocs = searcher.search(query, 10);
+            var indexReader = DirectoryReader.open(memoryIndex);
+            var searcher = new IndexSearcher(indexReader);
+            var topDocs = searcher.search(query, 10);
+
             List<Document> documents = new ArrayList<>();
             for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
                 documents.add(searcher.doc(scoreDoc.doc));
