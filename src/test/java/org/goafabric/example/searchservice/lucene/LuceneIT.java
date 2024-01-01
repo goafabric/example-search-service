a71fc929630e5bd8f7934661930cf425a29938a3
Andreas Mautsch <amautsch>
2024-01-01 11:25:21 +0100

    lucene added

--- src/test/java/org/goafabric/example/searchservice/lucene/LuceneIT.java
+++ src/test/java/org/goafabric/example/searchservice/lucene/LuceneIT.java
@@ -1,17 +1,21 @@
 package org.goafabric.example.searchservice.lucene;
 
-
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.FuzzyQuery;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+//https://www.baeldung.com/lucene
 public class LuceneIT {
 
     @Test
@@ -31,4 +35,19 @@ public class LuceneIT {
                 documents.get(0).get("title"));
     }
 
+
+    @Test
+    public void fuzzySearch() {
+        var inMemoryLuceneIndex = new InMemoryLuceneIndex(new ByteBuffersDirectory(), new StandardAnalyzer());
+
+        inMemoryLuceneIndex.indexDocument("article", "Halloween Festival");
+        inMemoryLuceneIndex.indexDocument("decoration", "Decorations for Halloween");
+
+        Term term = new Term("body", "hallowen");
+        Query query = new FuzzyQuery(term);
+
+        var documents = inMemoryLuceneIndex.searchIndex(query);
+        assertThat(documents).isNotEmpty();
+        documents.forEach(System.err::println);
+    }
 }
