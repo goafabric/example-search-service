46113d5bb96e02c6d582a3d0cddba4d1554bc63e
Andreas Mautsch <amautsch>
2024-01-01 11:35:35 +0100

    lucene added

--- src/test/java/org/goafabric/example/searchservice/lucene/LuceneIT.java
+++ src/test/java/org/goafabric/example/searchservice/lucene/LuceneIT.java
@@ -4,7 +4,6 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.FuzzyQuery;
-import org.apache.lucene.search.Query;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.junit.jupiter.api.Test;
 
@@ -43,11 +42,48 @@ public class LuceneIT {
         inMemoryLuceneIndex.indexDocument("article", "Halloween Festival");
         inMemoryLuceneIndex.indexDocument("decoration", "Decorations for Halloween");
 
-        Term term = new Term("body", "hallowen");
-        Query query = new FuzzyQuery(term);
+        var query = new FuzzyQuery(new Term("body", "hallowen"));
 
         var documents = inMemoryLuceneIndex.searchIndex(query);
         assertThat(documents).isNotEmpty();
         documents.forEach(System.err::println);
     }
+
+    //only works with single search terms, not complete name
+    @Test
+    public void fuzzyMueller() {
+        var inMemoryLuceneIndex = new InMemoryLuceneIndex(new ByteBuffersDirectory(), new StandardAnalyzer());
+        inMemoryLuceneIndex.indexDocument("article", "hans müller");
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "müller")))).hasSize(1);
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "mueller")))).hasSize(1);
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "miller")))).hasSize(1);
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "meyer")))).hasSize(0);
+    }
+
+    @Test
+    public void fuzzyMeyer() {
+        var inMemoryLuceneIndex = new InMemoryLuceneIndex(new ByteBuffersDirectory(), new StandardAnalyzer());
+        inMemoryLuceneIndex.indexDocument("article", "erich meyer");
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "meyer")))).hasSize(1);
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "meier")))).hasSize(1);
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "mayor")))).hasSize(1);
+
+        assertThat(inMemoryLuceneIndex.searchIndex(
+                new FuzzyQuery(new Term("body", "müller")))).hasSize(0);
+    }
+
 }
