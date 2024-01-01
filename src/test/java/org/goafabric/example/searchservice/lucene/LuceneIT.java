cc98b3a53f6a7afed0bee733bb2e7707ccc706f1
Andreas Mautsch <amautsch>
2024-01-01 11:20:30 +0100

    lucene added

--- src/test/java/org/goafabric/example/searchservice/lucene/LuceneIT.java
+++ src/test/java/org/goafabric/example/searchservice/lucene/LuceneIT.java
@@ -6,6 +6,7 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.store.ByteBuffersDirectory;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -14,9 +15,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LuceneIT {
 
     @Test
-    public void givenSearchQueryWhenFetchedDocumentThenCorrect() {
-        InMemoryLuceneIndex inMemoryLuceneIndex
-                = new InMemoryLuceneIndex(new ByteBuffersDirectory(), new StandardAnalyzer());
+    public void givenSearchQueryWhenFetchedDocumentThenCorrect() throws IOException {
+        var inMemoryLuceneIndex = new InMemoryLuceneIndex(new ByteBuffersDirectory(), new StandardAnalyzer());
+        //var inMemoryLuceneIndex  = new InMemoryLuceneIndex(new NIOFSDirectory(Path.of("/Users/andreas/Downloads/lucene")), new StandardAnalyzer());
+
         inMemoryLuceneIndex.indexDocument("Hello world", "Some hello world");
 
         List<Document> documents
